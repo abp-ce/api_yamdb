824fae4cc72ac3fa5484dc1971318b496ed6e613
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2022-05-30 22:22:57 +0500

    Test commit

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     USER = 'us'
     MODERATOR = 'mo'
-    ADMIN ='ad'
+    ADMIN = 'ad'
     ROLE_CHOICES = (
         ('us', 'user',),
         ('mo', 'moderator'),
