9bd41202f54280d405e09c217471f70813339951
Andrei Polyakov <ap120368@gmail.com>
2022-06-01 10:47:58 +0500

    Any changes

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from .viewsets import CreateViewSet
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(CreateViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        # send email
+        serializer.save(user=self.request.user)
