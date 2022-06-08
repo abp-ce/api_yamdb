9b6d637c2c844040ac89800af41735fab3d62c2d
Andrei Polyakov <ap120368@gmail.com>
2022-06-08 22:28:49 +0500

    Second review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,9 +7,9 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title
 from reviews.user import User
+
 from .filters import TitleFilter
 from .permissions import (AuthModeratorAdminOrReadOnly, IsAdminOrReadOnly,
                           IsAdminRoleOnly)
